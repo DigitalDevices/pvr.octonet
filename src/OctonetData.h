a19fef8af843dfe0b1ce62982fe09e58ee819bb4
Alwin Esch <alwin.esch@web.de>
2020-03-25 05:24:55 +0100

    update source license to use SPDX

--- src/OctonetData.h
+++ src/OctonetData.h
@@ -1,26 +1,14 @@
-#pragma once
-
 /*
- * Copyright (C) 2015 Julian Scheel <julian@jusst.de>
- * Copyright (C) 2015 jusst technologies GmbH
- * Copyright (C) 2015 Digital Devices GmbH
+ *  Copyright (C) 2015 Julian Scheel <julian@jusst.de>
+ *  Copyright (C) 2015 jusst technologies GmbH
+ *  Copyright (C) 2015 Digital Devices GmbH
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
- * USA.
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *  See LICENSE.md for more information.
  *
  */
+
+#pragma once
 
 #include <vector>
 
